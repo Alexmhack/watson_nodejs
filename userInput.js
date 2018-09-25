bf1b05beebb4bf6a3cd5d1a633955729c6961f2e
alexHooper <alexmohack@gmail.com>
2018-09-25 21:38:31 +0530

    process the intent in input

--- userInput.js
+++ userInput.js
@@ -14,3 +14,28 @@ var workspace_id = process.env.WORKSPACE_ID;
 service.message({
 	workspace_id: workspace_id
 }, processResponse);
+
+function processResponse(err, response) {
+	if (err) {
+		// in case something went wrong
+		console.error(err);
+		return;
+	}
+
+	// if an intent was detected log to console.
+	if (response.intents.length > 0) {
+		console.log("Intents Detected: " + response.intents[0].intent);
+	}
+
+	// display the output from dialog if any
+	if (response.output.text.length != 0) {
+		console.log(response.output.text[0]);
+	}
+
+	// prompt the next user input
+	var newMessageFromUser = prompt(">> ");
+	service.message({
+		workspace_id: workspace_id,
+		input: { text: newMessageFromUser }
+	}, processResponse)
+}
