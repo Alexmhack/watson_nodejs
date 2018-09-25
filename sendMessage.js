8a9aca9e31c38850bcd864560829853b5446279e
alexHooper <alexmohack@gmail.com>
2018-09-25 19:50:43 +0530

    send empty message to watson

--- sendMessage.js
+++ sendMessage.js
@@ -9,3 +9,20 @@ var service = new AssistantV1({
 })
 
 var workspaceId = process.env.WORKSPACE_ID;
+
+service.message({
+	workspace_id: workspaceId
+}, processResponse);
+
+function processResponse(err, response) {
+	if (err) {
+		// in case something went wrong.
+		console.error(err);
+		return;
+	}
+
+	// Display the output from dialog, if any
+	if (response.output.text.length != 0) {
+		console.log(response.output.text[0]);
+	}
+}
