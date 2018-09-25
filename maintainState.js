c059b62d28f4470fc707e1b46a40fa6f4411cd18
alexHooper <alexmohack@gmail.com>
2018-09-25 23:14:38 +0530

    send context along with each message

--- maintainState.js
+++ maintainState.js
@@ -13,3 +13,31 @@ var workspace_id = process.env.WORKSPACE_ID;
 service.message({
 	workspace_id: workspace_id	
 }, processResponse);
+
+// process the service response
+function processResponse(err, response) {
+	if (err) {
+		// in case of errors
+		console.error(err);
+		return;
+	}
+
+	// if an intent was detected log it on console
+	if (response.intents.length > 0) {
+		console.log("Detected Intents: " + response.intents[0].intent);
+	}
+
+	// Display the output from dialog if any
+	if (response.output.text.length != 0) {
+		console.log(response.output.text[0]);
+	}
+
+	// Prompt for the next round of input.
+    var newMessageFromUser = prompt('>> ');
+    // Send back the context to maintain state.
+    service.message({
+      workspace_id: workspace_id,
+      input: { text: newMessageFromUser },
+      context : response.context,
+    }, processResponse)
+}
